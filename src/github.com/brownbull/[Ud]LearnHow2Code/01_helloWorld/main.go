8df77ca28bd567dc1ef59738b01b0b0025a85201
unknown <carcamo.gabriel@gmail.com>
2020-12-02 21:42:28 -0300

    asd

--- src/github.com/brownbull/[Ud]LearnHow2Code/01_helloWorld/main.go
+++ src/github.com/brownbull/[Ud]LearnHow2Code/01_helloWorld/main.go
@@ -1,3 +1,14 @@
+// # GitBash - Windows
+// cd "src\github.com\brownbull\[Ud]LearnHow2Code\01_helloWorld"
+// # RUN
+// go run main.go
+// # BUILD
+// go build
+// # EXEC
+// ./01_helloWorld.exe
+// # INSTALL - GOBIN must be a valid env var, add to path too for direct exec
+// go install main.go
+// main.exe
 package main
 
 import (
